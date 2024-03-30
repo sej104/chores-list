160122b0cb5dbca86922f15af6f0ec690bc49b7f
Sergio Joya <sejoya104@gmail.com>
2024-03-30 09:34:44 -0400

    Add javascript file

---
+++ public/javascript/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Your JavaScript code here
+    document.getElementById('addTaskBtn').addEventListener('click', function() {
+        alertBox.classList.remove('d-none');
+    });
+});
